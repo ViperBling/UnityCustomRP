79eac27b36ba14862f4644e2a0e1e15e3b0fa27b
ArtoriasLeo <qiusongtao2010@hotmail.com>
2024-11-28 19:34:46 +0800

    [M] Backup

--- Packages/com.artorias.lite-render-pipelines/Runtime/RenderPasses/GeometryPass.cs
+++ Packages/com.artorias.lite-render-pipelines/Runtime/RenderPasses/GeometryPass.cs
@@ -19,38 +19,38 @@ namespace LiteRP
         private void AddGeometryPass(RenderGraph rg, ContextContainer frameData)
         {
             CameraData cameraData = frameData.Get<CameraData>();
-            using (var rgBuilder = rg.AddRasterRenderPass<GeometryPassData>(s_GeometryPassSampler.name, out var passData, s_GeometryPassSampler))
+            
+            using var rgBuilder = rg.AddRasterRenderPass<GeometryPassData>(s_GeometryPassSampler.name, out var passData, s_GeometryPassSampler);
+            
+            // 声明或引用资源
+            // 不透
+            RendererListDesc opaqueListDesc = new RendererListDesc(s_ShaderTagID, cameraData.m_CullingResults, cameraData.m_Camera);
+            opaqueListDesc.sortingCriteria = SortingCriteria.CommonOpaque;
+            opaqueListDesc.renderQueueRange = RenderQueueRange.opaque;
+            passData.opaqueRendererListHandle = rg.CreateRendererList(opaqueListDesc);
+            rgBuilder.UseRendererList(passData.opaqueRendererListHandle);
+            
+            // 半透
+            RendererListDesc transListDesc = new RendererListDesc(s_ShaderTagID, cameraData.m_CullingResults, cameraData.m_Camera);
+            transListDesc.sortingCriteria = SortingCriteria.CommonTransparent;
+            transListDesc.renderQueueRange = RenderQueueRange.transparent;
+            passData.transRendererListHandle = rg.CreateRendererList(transListDesc);
+            rgBuilder.UseRendererList(passData.transRendererListHandle);
+            
+            // 导入BackBuffer
+            passData.backBufferHandle = rg.ImportBackbuffer(BuiltinRenderTextureType.CurrentActive);
+            rgBuilder.SetRenderAttachment(passData.backBufferHandle, 0, AccessFlags.Write);
+            
+            // 设置全局渲染状态
+            rgBuilder.AllowPassCulling(false);
+            
+            // 设置渲染函数
+            rgBuilder.SetRenderFunc<GeometryPassData>((geoPassData, rgContext) =>
             {
-                // 声明或引用资源
-                // 不透
-                RendererListDesc opaqueListDesc = new RendererListDesc(s_ShaderTagID, cameraData.m_CullingResults, cameraData.m_Camera);
-                opaqueListDesc.sortingCriteria = SortingCriteria.CommonOpaque;
-                opaqueListDesc.renderQueueRange = RenderQueueRange.opaque;
-                passData.opaqueRendererListHandle = rg.CreateRendererList(opaqueListDesc);
-                rgBuilder.UseRendererList(passData.opaqueRendererListHandle);
-                
-                // 半透
-                RendererListDesc transListDesc = new RendererListDesc(s_ShaderTagID, cameraData.m_CullingResults, cameraData.m_Camera);
-                transListDesc.sortingCriteria = SortingCriteria.CommonTransparent;
-                transListDesc.renderQueueRange = RenderQueueRange.transparent;
-                passData.transRendererListHandle = rg.CreateRendererList(transListDesc);
-                rgBuilder.UseRendererList(passData.transRendererListHandle);
-                
-                // 导入BackBuffer
-                passData.backBufferHandle = rg.ImportBackbuffer(BuiltinRenderTextureType.CurrentActive);
-                rgBuilder.SetRenderAttachment(passData.backBufferHandle, 0, AccessFlags.Write);
-                
-                // 设置全局渲染状态
-                rgBuilder.AllowPassCulling(false);
-                
-                // 设置渲染函数
-                rgBuilder.SetRenderFunc<GeometryPassData>((geoPassData, rgContext) =>
-                {
-                    rgContext.cmd.DrawRendererList(passData.opaqueRendererListHandle);
-                    rgContext.cmd.DrawRendererList(passData.transRendererListHandle);
-                    
-                });
-            }
+                rgContext.cmd.DrawRendererList(passData.opaqueRendererListHandle);
+                rgContext.cmd.DrawRendererList(passData.transRendererListHandle);
+            });
+            
         }
     }
 }
