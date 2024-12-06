de6147fb10b0b08f7f62cd568242aeed272538fd
ArtoriasLeo <qiusongtao2010@hotmail.com>
2024-12-06 18:24:02 +0800

    [A] Shadow Feature.

---
+++ Packages/com.artorias.lite-render-pipelines/Runtime/FrameData/ShadowData.cs
@@ -0,0 +1,80 @@
+﻿using Unity.Collections;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace LiteRP
+{
+    public class ShadowData : ContextItem
+    {
+        /// <summary>
+        /// 是否开启主光源阴影
+        /// </summary>
+        public bool m_MainLightShadowEnable;
+        
+        /// <summary>
+        /// 是否支持主光源阴影
+        /// </summary>
+        public bool m_SupportMainLightShadow;
+
+        /// <summary>
+        /// 主光源阴影贴图宽度
+        /// </summary>
+        public int mainLightShadowmapWidth;
+        
+        /// <summary>
+        /// 主光源阴影贴图高度
+        /// </summary>
+        public int mainLightShadowmapHeight;
+
+        /// <summary>
+        /// 主光源阴影距离
+        /// </summary>
+        public float mainLightShadowDistance;
+        
+        /// <summary>
+        /// 级联阴影级数
+        /// </summary>
+        public int mainLightShadowCascadesCount;
+        
+        /// <summary>
+        /// 级联阴影划分
+        /// </summary>
+        public Vector3 mainLightShadowCascadesSplit;
+
+        /// <summary>
+        /// 级联阴影分级过渡
+        /// </summary>
+        public float mainLightShadowCascadeBorder;
+
+        /// <summary>
+        /// 软阴影
+        /// </summary>
+        public bool supportSoftShadows;
+        
+        /// <summary>
+        /// shadow map位数
+        /// </summary>
+        public int shadowmapDepthBits;
+
+        /// <summary>
+        /// 阴影Bias
+        /// </summary>
+        public Vector4 mainLightShadowBias;
+        
+        /// <summary>
+        /// shadowmap 分辨率
+        /// </summary>
+        public int mainLightShadowResolution;
+
+        internal int mainLightTileShadowResolution;
+        internal int mainLightRenderTargetWidth;
+        internal int mainLightRenderTargetHeight;
+        
+        // internal NativeArray<LightShadowCasterCullingInfo>
+        
+        public override void Reset()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Packages/com.artorias.lite-render-pipelines/Runtime/Utilities/ShadowUtils.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using Unity.Collections;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Experimental.Rendering;
+using UnityEngine.Rendering.RenderGraphModule;
+
+namespace LiteRP
+{
+    public struct ShadowSliceData
+    {
+        /// <summary>
+        /// The view matrix.
+        /// </summary>
+        public Matrix4x4 viewMatrix;
+
+        /// <summary>
+        /// The projection matrix.
+        /// </summary>
+        public Matrix4x4 projectionMatrix;
+
+        /// <summary>
+        /// The shadow transform matrix.
+        /// </summary>
+        public Matrix4x4 shadowTransform;
+
+        /// <summary>
+        /// The X offset to the shadow map.
+        /// </summary>
+        public int offsetX;
+
+        /// <summary>
+        /// The Y offset to the shadow map.
+        /// </summary>
+        public int offsetY;
+
+        /// <summary>
+        /// The maximum tile resolution in an Atlas.
+        /// </summary>
+        public int resolution;
+
+        /// <summary>
+        /// The shadow split data containing culling information.
+        /// </summary>
+        public ShadowSplitData splitData;
+
+        /// <summary>
+        /// Clears and resets the data.
+        /// </summary>
+        public void Clear()
+        {
+            viewMatrix = Matrix4x4.identity;
+            projectionMatrix = Matrix4x4.identity;
+            shadowTransform = Matrix4x4.identity;
+            offsetX = offsetY = 0;
+            resolution = 1024;
+        }
+    }
+
+    public struct LightShadowCullingInfos
+    {
+        public NativeArray<ShadowSliceData> shadowSlices;
+        public uint slicesValidMask;
+        
+        public readonly bool IsSliceValid(int i) => (slicesValidMask & (1u << i)) != 0; 
+    }
+}
