d614b12318e85ce8c3e7fd0aa1f559d9120d8a38
ArtoriasLeo <qiusongtao2010@hotmail.com>
2024-12-11 11:02:00 +0800

    [A] Add CameraGUI

---
+++ Packages/com.artorias.lite-render-pipelines/Editor/CameraGUI/LiteRPCameraEditor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+// 修复Camera.GetCommandBuffer Warning
+namespace LiteRP.Editor
+{
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(Camera))]
+    [SupportedOnRenderPipeline(typeof(LiteRPAsset))]
+    public class CustomCameraEditor : UnityEditor.Editor
+    {
+        private SerializedLiteRPCameraProperties serializedCameraProperties { get; set; }
+        
+        CameraEditor.Settings m_Settings;
+        protected CameraEditor.Settings settings => m_Settings ??= new CameraEditor.Settings(serializedObject);
+
+        public void OnEnable()
+        {
+            settings.OnEnable();
+            serializedCameraProperties = new SerializedLiteRPCameraProperties(serializedObject, settings);
+            Undo.undoRedoPerformed += ReconstructReferenceToAdditionalDataSO;
+        }
+        
+        public void OnDisable()
+        {
+            Undo.undoRedoPerformed -= ReconstructReferenceToAdditionalDataSO;
+        }
+
+        void ReconstructReferenceToAdditionalDataSO()
+        {
+            OnDisable();
+            OnEnable();
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedCameraProperties.Update();
+            LiteRPCameraGUIUtilities.Inspector.Draw(serializedCameraProperties, this);
+            serializedCameraProperties.Apply();
+        }
+    }
+}
